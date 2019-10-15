27e5e4b560dfc2cfd6d40c7a51379e190cbba3f6
Noah <noah.colbourne@ocsbstudent.ca>
2019-10-15 12:49:31 -0400

    yes

--- GuessNumberNoah/GuessNumberNoah/GuessNumberForm.Designer.cs
+++ GuessNumberNoah/GuessNumberNoah/GuessNumberForm.Designer.cs
@@ -28,20 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.picAnswer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picAnswer)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.Location = new System.Drawing.Point(60, 261);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(123, 55);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(56, 136);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(342, 24);
+            this.lblQuestion.TabIndex = 1;
+            this.lblQuestion.Text = "Guess a number between 1 and 10:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(252, 292);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(57, 20);
+            this.lblAnswer.TabIndex = 2;
+            this.lblAnswer.Text = "label2";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuess.Location = new System.Drawing.Point(563, 136);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(179, 38);
+            this.txtGuess.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(521, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // picAnswer
+            // 
+            this.picAnswer.Image = global::GuessNumberNoah.Properties.Resources.red_x;
+            this.picAnswer.Location = new System.Drawing.Point(549, 261);
+            this.picAnswer.Name = "picAnswer";
+            this.picAnswer.Size = new System.Drawing.Size(239, 158);
+            this.picAnswer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAnswer.TabIndex = 5;
+            this.picAnswer.TabStop = false;
+            this.picAnswer.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // frmGuessNumber
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picAnswer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnCheck);
             this.Name = "frmGuessNumber";
             this.Text = "Guess My Number by Noah.C";
+            this.Load += new System.EventHandler(this.frmGuessNumber_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picAnswer)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox picAnswer;
     }
 }
 
--- GuessNumberNoah/GuessNumberNoah/GuessNumberForm.cs
+++ GuessNumberNoah/GuessNumberNoah/GuessNumberForm.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+    * Created by: Noah Colbourne
+    * Created on: 6-Oct-2019
+    * Created for: ICS3U Programming
+    * Daily Assignment – Day #16 - Guess Number
+    * This program Takes the users guess and will tell
+    * them if they guessued the right number.
+   */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +23,58 @@ namespace GuessNumberNoah
         public frmGuessNumber()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmGuessNumber_Load(object sender, EventArgs e)
+        {
+            this.lblAnswer.Hide();
+
+            this.picAnswer.Hide();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            const int NUMBER = 4;
+            const int MAX = 10;
+            const int MIN = 1;
+            int GuessNumber;
+
+            GuessNumber = int.Parse(txtGuess.Text);
+
+            if (GuessNumber == NUMBER)
+            {   lblAnswer.Show();
+                picAnswer.Show();
+                lblAnswer.Text = "CORRECT!";
+                picAnswer.Image = Properties.Resources.checkmark;
+            }
+            else if (GuessNumber > MAX)
+                {
+                lblAnswer.Show();
+                picAnswer.Show();
+                lblAnswer.Text = "Please use a smaller number!";
+                picAnswer.Image = Properties.Resources.red_x; }
+            else if (GuessNumber < MIN)
+            {
+                lblAnswer.Show();
+                picAnswer.Show();
+                lblAnswer.Text = "Please use a bigger number!";
+                picAnswer.Image = Properties.Resources.red_x;
+            }
+            else
+            {
+                lblAnswer.Show();
+                picAnswer.Show();
+                lblAnswer.Text = "WRONG!";
+                picAnswer.Image = Properties.Resources.red_x;
+            }
+
+
+
         }
     }
 }
