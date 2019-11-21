5ec59f81a60e0b7a79a567425fe60ea94fbad757
Noah <noah.colbourne@ocsbstudent.ca>
2019-11-21 13:07:08 -0500

    yes

---
+++ Walking Man Noah/Walking Man Noah/WalkingManForm.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Walking_Man_Noah
+{
+    partial class WalkingManForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.picWalkingMan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkingMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(355, 189);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // picWalkingMan
+            // 
+            this.picWalkingMan.Image = global::Walking_Man_Noah.Properties.Resources.walk10;
+            this.picWalkingMan.Location = new System.Drawing.Point(533, 95);
+            this.picWalkingMan.Name = "picWalkingMan";
+            this.picWalkingMan.Size = new System.Drawing.Size(186, 214);
+            this.picWalkingMan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picWalkingMan.TabIndex = 2;
+            this.picWalkingMan.TabStop = false;
+            this.picWalkingMan.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // WalkingManForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picWalkingMan);
+            this.Controls.Add(this.btnStart);
+            this.Name = "WalkingManForm";
+            this.Text = "Walking Man by Noah";
+            this.Load += new System.EventHandler(this.WalkingManForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkingMan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox picWalkingMan;
+    }
+}
+
---
+++ Walking Man Noah/Walking Man Noah/WalkingManForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Walking_Man_Noah
+{
+    public partial class WalkingManForm : Form
+    {
+        public WalkingManForm()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            const byte MAX_FRAMES = 10;
+            int pictureFrameCounter = 1;
+
+            while (pictureFrameCounter < MAX_FRAMES + 1)
+            {
+                if (pictureFrameCounter == 1)
+                {
+                    this.picWalkingMan.Image = Properties.Resources.walk1;
+                }
+                else if (pictureFrameCounter == 2)
+                {
+                    this.picWalkingMan.Image = Properties.Resources.walk2;
+                }
+                else if (pictureFrameCounter == 3)
+                {
+                    this.picWalkingMan.Image = Properties.Resources.walk3;
+                }
+                else if (pictureFrameCounter == 4)
+                {
+                    this.picWalkingMan.Image = Properties.Resources.walk4;
+                }
+                else if (pictureFrameCounter == 5)
+                {
+                    this.picWalkingMan.Image = Properties.Resources.walk5;
+                }
+                else if (pictureFrameCounter == 6)
+                {
+                    this.picWalkingMan.Image = Properties.Resources.walk6;
+                }
+                else if (pictureFrameCounter == 7)
+                {
+                    this.picWalkingMan.Image = Properties.Resources.walk7;
+                }
+                else if (pictureFrameCounter == 8)
+                {
+                    this.picWalkingMan.Image = Properties.Resources.walk8;
+                }
+                else if (pictureFrameCounter == 9)
+                {
+                    this.picWalkingMan.Image = Properties.Resources.walk9;
+                }
+                else
+                {
+                    this.picWalkingMan.Image = Properties.Resources.walk10;
+                }
+            }
+
+            this.Refresh();
+
+            pictureFrameCounter = pictureFrameCounter + 1;
+
+            Thread.Sleep(100);
+        }
+    }
+}
